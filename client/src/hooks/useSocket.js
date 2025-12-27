91e1ef1dfe2ede7f36e60cb821b64a53959b8355
aasif41 <mdaasif18092003@gmail.com>
2025-12-27 20:32:49 +0530

    Fix CORS and 404 error with vercel.json

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // Yahan humne socket connection setup kiya hai.
 // Ye URL aapke server ke port (5000) se match hona chahiye.
-const socket = io.connect("https://watch-party-app-server.onrender.com");
+const socket = io.connect("https://watch-party-app-1-fco3.onrender.com");
 
 export const useSocket = () => {
   return socket;
